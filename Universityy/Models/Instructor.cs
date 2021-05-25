c9698757b4fdf94ba84e0d84b00a0611ffa011d3
juan jose vilchez barzola <juan_vb_1998@hotmail.com>
2021-05-25 18:54:50 -0500

    Agregar archivos de proyecto.

---
+++ Universityy/Models/Instructor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Universityy.Models
+{
+    public class Instructor : Person
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime HiretDate { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+        public virtual OfficeAssignment OfficeAssigment { get; set; }
+    }
+}
---
+++ Universityy/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Web;
+using Universityy.Models;
+
+namespace Universityy
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("name = MyContextDB")
+        { 
+        
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<Person> People { get; set; }
+
+
+
+    }
+}
